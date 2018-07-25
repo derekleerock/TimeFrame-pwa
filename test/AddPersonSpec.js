98a5e881360f7a553025b9f2ff6015704c26afd7
Canyon Boak <immenor@gmail.com>
2018-07-25 10:38:00 +0900

    fill out tests for addPerson screen

--- test/AddPersonSpec.js
+++ test/AddPersonSpec.js
@@ -1,9 +1,25 @@
 import {mountAppAtLocation} from './mountAtLocation'
 
 describe('add person screen', () => {
-  it('displays a close button on the add new person screen', () => {
-    const app = mountAppAtLocation('/addPerson')
+  let addPersonScreen
 
-    expect(app.find('a.close').props().href).toEqual('/')
+  beforeEach(() => {
+    addPersonScreen = mountAppAtLocation('/addPerson')
+  })
+
+  it('displays a close button on the add new person screen', () => {
+    expect(addPersonScreen.find('a.close').props().href).toEqual('/')
+  })
+
+  it('displays the screen title', () => {
+    expect(addPersonScreen.text()).toContain('Add A Person')
+  })
+
+  it('displays the label for the persons name', () => {
+    expect(addPersonScreen.text()).toContain('Name')
+  })
+
+  it('displays the input field with a placeholder', () => {
+    expect(addPersonScreen.find('input').props().placeholder).toEqual('enter a name')
   })
 })
